f8f21946fa799c5784894dde7ae1a7a7f58070fc
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-01-07 17:59:48 +0100

    ag grid work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
